73379514f7b47e15914acb8a73d978a06b52a422
James Kurtz <jkrrv@jkrrv.org>
2015-01-15 12:04:36 -0500

    initializing stuff

--- CollegiateLink.class.php
+++ CollegiateLink.class.php
@@ -1,11 +1,32 @@
 <?php
 
-if (HAS_ACURL_VERSION < 2.1) {
+if (!defined('HAS_ACURL_VERSION')) {
 	require_once "libs/aCurl/aCurl.php";
 }
 
-class CollegiateLink {
-	public function __construct($cookieFile) {
+if (!defined('HDOM_TYPE_ELEMENT')) {
+	require_once "libs/php-dom-parser/php-dom-parser.php";
+}
 
+class CollegiateLink {
+	private $_status = 0;
+	private $_cookieFile;
+	private $_domain;
+
+
+	public function __construct($domain, $cookieFile) {
+		/* It is assumed that the user is already authenticated. */
+		$this->_domain = $domain;
+		$this->_cookieFile = $cookieFile;
+	}
+
+
+	public function getStatus() {
+		return $this->_status;
+	}
+
+	public function getOrganization($orgReference) {
+		include "CollegiateLinkOrganization.class.php";
+		return new CollegiateLinkOrganization($orgReference);
 	}
 }
